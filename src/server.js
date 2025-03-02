db47b3bb45a7941a30c6497997e1d0616d768364
SeanKervick <20104300@mail.wit.ie>
2025-03-02 12:20:30 +0000

    allow frontend domain as origin

--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ import { connectMongo } from "./connect.js";
 import routes from "./routes.js";
 
 const app = express(); // initialize express
+const origins = [
+  "https://data-heist-seank.vercel.app/", // frontend domain
+  "http://localhost:5173",
+];
 
 // enable CORS - frontend<->backend communication - only from localhost:5173
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origins }));
 // add middleware for JSON payloads
 app.use(express.json());
 // API routes
