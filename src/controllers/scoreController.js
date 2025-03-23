9b2e5086bb05a3ea19c787e305c72464c88ef2af
SeanKervick <20104300@mail.wit.ie>
2025-03-23 16:01:24 +0000

    getLeaderboard function & route added

--- src/controllers/scoreController.js
+++ src/controllers/scoreController.js
@@ -10,7 +10,7 @@ export const updateHighScore = async (req, res) => {
     const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    // only update the score if higher than score saved
+    // only update the score if higher than the user's previously saved score
     if (score > (user.score || 0)) {
       user.score = score;
       await user.save();
@@ -19,5 +19,16 @@ export const updateHighScore = async (req, res) => {
     res.status(200).json({ message: "Score updated", highScore: user.score });
   } catch (err) {
     res.status(500).json({ error: "Failed to update score" });
+  }
+};
+
+export const getLeaderboard = async (req, res) => {
+  try {
+    // get top 10 users, sort them by score in descending order (-1) https://www.mongodb.com/docs/manual/reference/method
+    const topUsers = await User.find().sort({ score: -1 }).limit(10);
+    res.status(200).json(topUsers);
+  } catch (err) {
+    console.error("Error fetching leaderboard:", err);
+    res.status(500).json({ error: "Failed to fetch leaderboard" });
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { accountsController } from "./controllers/accountsController.js";
 import { verifyToken } from "./middleware/jwt.js";
-import { updateHighScore } from "./controllers/scoreController.js";
+import { updateHighScore, getLeaderboard } from "./controllers/scoreController.js";
 
 const router = express.Router();
 
 //public routes
 router.post("/signup", accountsController.signup);
 router.post("/login", accountsController.login);
+router.get("/leaderboard", getLeaderboard);
+
 
 // protected routes
 router.get("/users", verifyToken, accountsController.getAllUsers);
